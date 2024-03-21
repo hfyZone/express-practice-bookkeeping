f65a24c19eb63a2d1166009115b61467045bbbcf
hfyZone <1178805691@qq.com>
2024-03-21 22:58:09 +0800

    账单增加

--- bookkeeping/routes/index.js
+++ bookkeeping/routes/index.js
@@ -12,7 +12,8 @@ const shortid = require('shortid')
 
 /* 记账本列表 */
 router.get('/account', function(req, res, next) {
-  res.render('list')
+  let accounts = db.get('accounts').value()
+  res.render('list', {accounts: accounts})
 });
 
 /* 添加记录 */
@@ -22,7 +23,7 @@ router.get('/create', function(req, res, next) {
 // 新增记录 用于对接create.ejs的form表单
 router.post("/account", (req, res) => {
   let id = shortid.generate()
-
+  console.log(req.body)
   db.get('accounts').unshift({id:id, ...req.body}).write();
   res.render('success', {msg: '添加成功', url: '/account'})
 })
