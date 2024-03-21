c46de94f7d817ded72a4119494dbbb6ee6b3cedc
hfyZone <1178805691@qq.com>
2024-03-21 21:34:54 +0800

    前后数据连通

--- bookkeeping/routes/index.js
+++ bookkeeping/routes/index.js
@@ -10,5 +10,10 @@ router.get('/account', function(req, res, next) {
 router.get('/create', function(req, res, next) {
   res.render('create')
 });
+// 新增记录 用于对接create.ejs的form表单
+router.post("/account", (req, res) => {
+  console.log(req.body)
+  res.send("添加记录")
+})
 
 module.exports = router;
